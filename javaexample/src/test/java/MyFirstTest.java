ceab51c31f5f2f4229013fdae6bfd812642db74f
tatizagrebina <zagrebina708@gmail.com>
2018-05-15 18:24:51 +0300

    Создание инфраструктуры, создание репозитория
    Создание инфраструктуры, создание репозитория

--- javaexample/src/test/java/MyFirstTest.java
+++ javaexample/src/test/java/MyFirstTest.java
@@ -11,34 +11,6 @@ public class MyFirstTest extends TestBase {
     @Test
     public void myFirstTest() {
         driver.navigate().to("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
     }
 
-    @Test
-    public void mySecondTest() {
-        driver.navigate().to("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
-    }
-
-    @Test
-    public void myThirdTest() {
-        driver.navigate().to("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).click();
-        wait.until(titleIs("webdriver - Поиск в Google"));
-    }
-
-    @Test
-    public void test10() {
-        driver.navigate().to("http://www.google.com");
-        driver.findElement(By.name("q")).sendKeys("webdriver");
-        driver.findElement(By.name("btnK")).sendKeys(Keys.ENTER);
-        wait.until(titleIs("webdriver - Поиск в Google"));
-    }
-
-    }
 }
